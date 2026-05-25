002f5ff8c52fee668ee51f42700cc9182b1b8a12
engshernandezm <engshernandezm@gmail.com>
2026-05-24 23:58:29 -0600

    feat api.js

--- frontend/assets/js/api.js
+++ frontend/assets/js/api.js
@@ -1,7 +1,13 @@
 // Cliente API centralizado para FonzControl doitv3
 
 const API = (() => {
-  const BASE = window.API_URL || (window.location.protocol + '//' + window.location.hostname + ':3000');
+  const BASE = window.API_URL || (function () {
+    var port = window.location.port;
+    var isStandardPort = !port || port === '80' || port === '443';
+    return isStandardPort
+      ? window.location.protocol + '//' + window.location.hostname
+      : window.location.protocol + '//' + window.location.hostname + ':3000';
+  }());
 
   function token() { return localStorage.getItem('fc_token'); }
   function user()  { return JSON.parse(localStorage.getItem('fc_user') || 'null'); }
